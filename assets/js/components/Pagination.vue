a4d3f0865db6c1631a2f46d0ba8df4357408e04f
Robert La Rocca <rlarocca@shapeways.com>
2019-04-22 10:13:12 -0400

    Added missing vue component

---
+++ assets/js/components/Pagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="index in parseInt(totalPages)">
+        <a :href="baseUrl + '&page=' + index">
+          <span v-if="index == page" class="selected">{{ index }}</span>
+          <span v-else>{{index}}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Pagination",
+    props: {
+      baseUrl: {
+        required: true
+      },
+      page: {
+        required: true
+      },
+      totalPages: {
+        required: true
+      },
+    },
+    methods: {
+    },
+  }
+</script>
+
+<style scoped>
+  li {
+    display: inline-block;
+    padding: 2px;
+  }
+  .selected {
+    color:green;
+  }
+  a {
+    color: #000000;
+  }
+</style>
